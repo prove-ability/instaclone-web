212a4cb86d38aee6f31e1785a50aa53e4cc0f9e7
Prove <prove.ability00@gmail.com>
2021-05-17 09:06:14 +0900

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useReactiveVar } from "@apollo/client";
+import { gql, useMutation, useReactiveVar } from "@apollo/client";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 
@@ -7,10 +7,30 @@ import Login from "./screens/Login";
 import NotFound from "./screens/NotFound";
 import { isLoggedInVar, darkModeVar } from "./apollo";
 import { darkTheme, lightTheme, GlobalStyles } from "./styles";
+import { login, loginVariables } from "./__generated__/login";
+
+const LOGIN_MUTATION = gql`
+  mutation login($username: String!, $password: String!) {
+    login(username: $username, password: $password) {
+      ok
+      token
+      error
+    }
+  }
+`;
 
 function App() {
   const isLoggedIn = useReactiveVar(isLoggedInVar);
   const darkMode = useReactiveVar(darkModeVar);
+  // const onCompleted =
+  const [loginMutation] = useMutation<login, loginVariables>(LOGIN_MUTATION, {
+    variables: {
+      username: "test",
+      password: "test",
+    },
+    onCompleted: (data) => data.login.token,
+  });
+
   return (
     <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
       <GlobalStyles />
